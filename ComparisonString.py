26fc52a2b3bf3b799b1419bc3e9144af69f04cee
Mustafa <mstfclb2008@gmail.com>
2023-07-26 14:20:18 +0300

    Publishing Commit

---
+++ ComparisonString.py
@@ -0,0 +1,28 @@
+#'== Operator'(Equality Test)
+
+print("Apple" == "Apple")
+#True
+
+print("Apple" == "Banana")
+#False
+
+
+#'!= Operator'(Inequality)
+
+print("Apple" != "Banana")
+#True
+
+print("Banana" != "Banana")
+#False
+
+
+#'< Operator'(Smallness)
+
+print("a" < "b")
+#True
+
+print("aba" < "abc")
+#True
+
+print("vyz" < "abc")
+#False
