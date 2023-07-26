a9c26d4bab4c40fa7eca6e06080c60acfc98c8a7
Mustafa <mstfclb2008@gmail.com>
2023-07-26 13:19:29 +0300

    -New work file added

---
+++ ComparisoninNumericData.py
@@ -0,0 +1,66 @@
+#'== Operator'(Equality Test)
+
+x = 10
+y = 10
+print(x == y)
+#True
+
+z = 25
+t = 30
+print(z == t)
+#False
+
+
+#'!= Operator'(Inequality Test)
+
+a = 86
+b = 102
+c = 30
+d = 30
+
+print(a != b) 
+#True
+
+print(c != d)
+#False
+
+print(type(a != b))
+#Boolean
+
+
+#'> Operator'(Bigness)
+
+print(7 > 8)
+#False
+
+print(8 > 7)
+#True
+
+
+#'>= Operator'(Bigness-Equality)
+
+print(6 >= 6)
+#True
+
+print(6 >= 8)
+#False
+
+
+#'<'(Smallness)
+
+print(3 < 2)
+#False
+
+print(3 < 5)
+#True
+
+
+#'<='(Smallness-Equality)
+
+print(2 <= 2)
+#True
+
+print(2 <= 1)
+#False
+
+#THAT'S ALL! :)
