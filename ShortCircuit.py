3020e55eb0e120d73d02b579b6ae51f195853eb5
Mustafa <mstfclb2008@gmail.com>
2023-07-27 10:25:29 +0300

    All subjects added

---
+++ ShortCircuit.py
@@ -0,0 +1,12 @@
+#If computer see False at first word in 'and Operator' it doesn't look second word and it says always False. There can be anythink at second word. Like this:
+
+print(False and 5 > 6)
+#False
+
+print(not True and True)
+#False
+
+
+#If compure see True at first word in 'or Operator' it doestn' look other words and it says always True like this:
+
+print(True and 'anythink')
