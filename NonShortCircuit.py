b4764c06fd3d72e8e58e3b106f884c3b2e1ea2b4
Mustafa <mstfclb2008@gmail.com>
2023-07-27 10:50:09 +0300

    It's ready

---
+++ NonShortCircuit.py
@@ -0,0 +1,10 @@
+#This operators provide to codes be non short-circuit. You can use it if you want your codes be non short-circuit. 
+
+print(False | 5 > 3)
+#False
+
+(5 > 3) | print("hey")
+(5 > 3) & print("hey")
+#This operators printing hey but take a mistake.
+
+#I think this operators dont useful but everybody can't accept my opinion. 
