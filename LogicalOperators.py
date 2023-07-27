147ad0fa1b9ac28c316a24a2cb87c62bb180b74a
Mustafa <mstfclb2008@gmail.com>
2023-07-27 09:53:26 +0300

    or Operator added

--- LogicalOperators.py
+++ LogicalOperators.py
@@ -26,3 +26,18 @@ print(False and False)
 
 print(5 > 2 and 3 < 1)
 #False
+
+
+#'or Operator'
+
+print(True or False)
+#True
+
+print(True or True)
+#True
+
+print(False or False)
+#False
+
+print(8 == 7 or 6 >= 6)
+#True
