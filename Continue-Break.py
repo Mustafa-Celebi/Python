6aa31f1ce519df5979043083d5ef6b3deb588703
unknown <mstfclb2008@gmail.com>
2023-08-11 23:21:00 +0300

    Break added

--- Continue-Break.py
+++ Continue-Break.py
@@ -12,8 +12,13 @@ while x < 10:
     x += 1
     if x == 3:
         break
-    
     print(x)
 
 
-#It provides
+#It provides to skip a thing like this:
+
+for i in range(10):
+
+    if i == 3:
+        continue
+    print(i)
