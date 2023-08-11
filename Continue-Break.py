a2dfbf12731a151f82e70c6c6111e1014d64705b
unknown <mstfclb2008@gmail.com>
2023-08-11 23:12:26 +0300

    Break added

---
+++ Continue-Break.py
@@ -0,0 +1,19 @@
+#It provides we can pause the loop (Break)
+
+for i in range(10):
+    if i == 3:
+        break
+    print(i)
+
+
+x = 0
+
+while x < 10:
+    x += 1
+    if x == 3:
+        break
+    
+    print(x)
+
+
+#It provides
